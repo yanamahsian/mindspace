6cd5bd324cc684584f40711e31368a428c720af7
yanamahsian <olegovnaianina@gmail.com>
2025-12-26 13:34:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -104,3 +104,9 @@
   const saved = localStorage.getItem('anki_nav_open');
   if (saved === '1') open();
 })();
+// PWA: регистрация Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js");
+  });
+}
